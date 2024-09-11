a87bc784134e2438c5e5ff3b404efe2bfc29cc9f
anjankumardas5 <anjantechnology507@gmail.com>
2024-09-11 22:45:58 +0530

    js day 1

---
+++ Shradha_Didi/first.js
@@ -0,0 +1,43 @@
+console.log("Jay shree Ram");
+age=23;
+console.log(age);
+fullname=" Anjan Kumar Das";
+console.log(fullname);
+isFollow=true;
+console.log(isFollow);
+let price=234;
+console.log(price);
+console.log(typeof price);
+let x=null;
+console.log(typeof x);
+
+const Student={
+  FullName:"Anjan",
+  age:23,
+  cgpa:8.39,
+  isPass:true,
+
+};
+console.log(Student);
+console.log(Student["FullName"]);
+
+
+
+const Product={
+  tiltle:"Ball Pen",
+  rating:4,
+  offer:5,
+  price:270
+}
+console.log(Product)
+
+
+const ProfilePhoto={
+  Name:"Sharadha",
+  isFollow:true,
+  TotalFollowers:1556,
+  TotalFollowing:4,
+
+}
+console.log(ProfilePhoto)
+console.log(ProfilePhoto["Name"])

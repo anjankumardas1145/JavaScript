8e56241f5293845d3ccc60f873bf9f52ebf64713
anjankumardas5 <anjantechnology507@gmail.com>
2024-09-18 19:32:48 +0530

    loop in js

---
+++ Shradha_Didi/day_3.js
@@ -0,0 +1,73 @@
+// for(let count=1;count <=10000;count++){
+//   console.log("Ankita");
+// }
+
+
+//  let sum=0;
+// for(let i=1;i<=10;i++){
+//   sum=sum+i;
+// }
+// console.log(sum);
+
+//WHILE LOOP
+// let i=1;
+// while(i<=9){
+//   console.log("An. Das");
+//   i++;
+// }
+
+// let i=20;
+// do{
+//   console.log("An. Das");
+//   i++;}
+//   while(i<=10);
+
+//   //for-of loop
+//   let str="Anjan Das";
+//   for (const i of str) {
+//     console.log("i=",i)
+//   }
+
+  //for_ in
+
+  // let Student={
+  //   name:"Anjan",
+  //   age:22,
+  //   cgpa:3.2,
+  //   isPass:true,
+
+  // };
+  // for (let key in Student ) {
+    
+  //   console.log("key =",key,
+  //     "value=",Student[key]) ; 
+  //   }
+  
+
+
+  //practice question:1
+
+//  for (let i=1;i<=100;i++){
+//     if(i%2===0){
+//       console.log(i)
+//     }
+//   }
+
+
+//Question:2
+
+// let gameNumber=25;
+// let userInput=prompt("Guess the number:");
+// while(gameNumber!=userInput){
+// userInput=prompt("you entered wrong number so try again")
+// }
+// console.log("congrats Anjan you won")
+
+
+
+//String question
+
+let  fullName=prompt("Enter the name:");
+console.log(fullName);
+let username="@"+fullName+fullName.length;
+console.log(username)

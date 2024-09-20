92530ec990b4f912cde21fd725cde24f434c2cbf
anjankumardas5 <anjantechnology507@gmail.com>
2024-09-20 23:49:07 +0530

    array in js

---
+++ Shradha_Didi/day_4.js
@@ -0,0 +1,22 @@
+
+console.log(" Please god take care of her and you know she is just like a small kid who does not know how to face any crtical situation.Not like that she is unable to take decisions no no no she is so much mature but simple .She know everything but as an introvert it is difficult to express in front of others.yahh this is from my experience ..Okk At the end it's my humble request to that you would take care of her as a sister ,as a mother ,as daughter please please ")
+  
+//Array 
+ let arr=["Ankita","virat","Rohit","Ritika "];
+for(let i=0;i<arr.length;i++){
+  console.log(arr[i])
+}
+
+ console.log(arr);
+ console.log(arr.length);
+
+  let fav=[" Rabi","Junaid"," kaniska"," shusti","Shubham kumar"];
+  console.log(fav)
+ fav[1]="Mamata Yadev";//we can replace an in dex value like this
+  for(let i=0;i<fav.length;i++){
+  console.log(fav[i] )
+  }
+
+//  console.log(fav);
+//  console.log( typeof fav);
+ 

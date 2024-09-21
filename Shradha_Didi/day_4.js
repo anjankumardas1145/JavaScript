094881f8cb970e4a4dab6247225119690900c03f
anjankumardas5 <anjantechnology507@gmail.com>
2024-09-21 23:14:40 +0530

    array in js

--- Shradha_Didi/day_4.js
+++ Shradha_Didi/day_4.js
@@ -1,22 +1,60 @@
 
-console.log(" Please god take care of her and you know she is just like a small kid who does not know how to face any crtical situation.Not like that she is unable to take decisions no no no she is so much mature but simple .She know everything but as an introvert it is difficult to express in front of others.yahh this is from my experience ..Okk At the end it's my humble request to that you would take care of her as a sister ,as a mother ,as daughter please please ")
+console.log(" Please god take care of her and you know she is just like a small kid who does not know how to face any crtical situation.Not like that she is unable to take decisions no no no she is so much mature but simple .She knows everything but as an introvert it is difficult to express in front of others.yahh this is from my experience ..Okk At the end it's my humble request to that you would take care of her as a sister ,as a mother ,as daughter please please ")
   
 //Array 
- let arr=["Ankita","virat","Rohit","Ritika "];
-for(let i=0;i<arr.length;i++){
-  console.log(arr[i])
-}
+//  let arr=["Ankita","virat","Rohit","Ritika "];
+// for(let i=0;i<arr.length;i++){
+//   console.log(arr[i])
+// }
 
- console.log(arr);
- console.log(arr.length);
+//  console.log(arr);
+//  console.log(arr.length);
 
-  let fav=[" Rabi","Junaid"," kaniska"," shusti","Shubham kumar"];
-  console.log(fav)
- fav[1]="Mamata Yadev";//we can replace an in dex value like this
-  for(let i=0;i<fav.length;i++){
-  console.log(fav[i] )
-  }
+//   let fav=[" Rabi","Junaid"," kaniska"," shusti","Shubham kumar"];
+//   console.log(fav)
+//  fav[1]="Mamata Yadev";//we can replace an in dex value like this
+//   for(let i=0;i<fav.length;i++){
+//   console.log(fav[i] )
+//   }
+//   console.log("Jay jagannath")
+
+//   //for of
+//   for(let an of fav){
+//     console.log("An...",an)
+//   }
 
 //  console.log(fav);
 //  console.log( typeof fav);
- 
+//question:01
+// let marks=[10,20,30,40,50];
+// let sum=0;
+// for(let mark of marks){
+// sum=sum+mark;
+// }
+// let avg=0;
+// avg=sum/marks.length;
+// console.log(sum);
+// console.log(`Average marks of the class is ${avg}`);
+
+ //question:02
+
+//  let item=[250,645,300,900,50];
+//  let i=0;
+//  for(let val of item){
+//   console.log(`value at index ${i}=${val}`);
+  
+//   let offer=val/10;
+//   item[i]=item[i]-offer;
+//   console.log(`value after offer = $ {item[i]}`);
+//   i++;
+
+//  }
+
+
+let item=[250,645,300,900,50];
+
+  for(let i=0;i<item.length;i++){
+    let offer=item[i]/10;
+  item[i]=item[i]-offer;
+  console.log(`value after the offer is ${item[i]}`)
+}
